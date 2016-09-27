5c60758a55d55b92ee5dddf7e16e4c1af3b182a0
phil <phil@DESKTOP-NRR8LDI>
2016-09-28 01:41:59 +0200

    Add project files.

---
+++ Project1/ScriptTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAPFEWSELib;
+
+namespace HelloWorld
+{
+    class ScriptTest {
+        //--------------------------//
+        //main method somewhere else 
+        public static void Main(string[] args)
+        {
+            SapGuiScript script = new SapGuiScript();
+            GuiSession session = script.getSession();
+            ISapCTextField component = (ISapCTextField)session.FindById("/app/con[0]/ses[0]/wnd[0]/usr/ctxtSEOCLASS-CLSNAME");
+            component.Text = "ac";
+            
+        }
+    }
+}

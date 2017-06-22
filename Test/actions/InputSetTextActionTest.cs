4675fa78702578f66852fd0cdb741561f5b4374f
phil <phil@DESKTOP-NRR8LDI>
2017-06-22 03:33:50 +0200

    nix eigetlich

--- Test/actions/InputSetTextActionTest.cs
+++ Test/actions/InputSetTextActionTest.cs
@@ -9,11 +9,13 @@ namespace Test.actions {
         public void TestSimple() {
             FirstSessionProvider provider = new FirstSessionProvider();
             var contextMock = new Mock<ActionContext>();
+            var textMock = new Mock<TextProvider>();
             FirstSessionProvider sessionProvider = new FirstSessionProvider();
             contextMock.Setup(context => context.GetSession()).Returns(sessionProvider.GetSession());
+            textMock.Setup(text => text.GetText()).Returns("ABC");
 
-
-            //InputSetTextAction action = new InputSetTextAction("abc", );
+            InputSetTextAction action = new InputSetTextAction(textMock.Object, "/app/con[0]/ses[0]/wnd[0]/usr/txtRSYST-BNAME");
+            action.Execute(contextMock.Object);
 
         }
     }
